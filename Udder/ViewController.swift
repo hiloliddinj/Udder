52539f1bd0d1e50167a5398831ea47bc7776f3fc
Hiloliddin Jaloliddinzoda <hiloliddin.j@gmail.com>
2020-06-08 15:02:30 +0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Hiloliddin Jaloliddinzoda on 6/8/20.
+//  Copyright © 2020 Hiloliddin Jaloliddinzoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
