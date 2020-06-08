5313dca5e895bb29f01a201a5f66bbcb23652573
Hiloliddin Jaloliddinzoda <hiloliddin.j@gmail.com>
2020-06-08 15:36:53 +0500

    Print hello world added

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("Hello World")
+    }
 
 
 }
